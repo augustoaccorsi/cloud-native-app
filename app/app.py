92db079e95186f1ec23d696dde27627b8edf586f
augustoaccorsi <augusto.acorsi@gmail.com>
2021-04-08 12:49:23 -0300

    add hello world test

--- app/app.py
+++ app/app.py
@@ -15,9 +15,9 @@ def home():
 def prof():
     return header_text + description + professor, 200
 
-@app.route("/app/teste", methods=['GET'])
-def teste():
-    return "teste", 200
+@app.route("/app/hello", methods=['GET'])
+def hello():
+    return "Hello World ", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
